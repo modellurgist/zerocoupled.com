b21faf603fd5dde867defe0e2c57f239edf1f001
Tom Calloway <modellurgist@gmail.com>
2024-05-09 17:36:52 +0100

    more configuration

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,14 +3,38 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
-  logo: <span>Zero Coupled Architecture (ALA)</span>,
-  project: {
-    link: 'https://github.com/modellurgist/zerocoupled.com',
+  // Navigation - Header & Footer
+  banner: {
+    text: "This documentation is a Work-in-Progress. Estimated completion by July 2024."
   },
   docsRepositoryBase: 'https://github.com/modellurgist/zerocoupled.com',
   footer: {
     text: '© 2024 Tom Calloway (github.com/modellurgist)',
   },
+  logo: <span>Zero Coupled Architecture (ALA)</span>,
+  project: {
+    link: 'https://github.com/modellurgist/zerocoupled.com',
+  },
+  // Navigation - Sidebar
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true
+  },
+  // editLink: {
+  // // results in console error:
+  //   component: null
+  // },
+  feedback: {
+    content: "What could be better? Tell us here →",
+    labels: "reader-suggestion"
+  },
+  // Navigation - Table of Contents
+  toc: {
+    // backToTop: true
+    float: true,
+    title: "Current Page Contents"
+  },
+  // Page Metadata
   useNextSeoProps: () => {
     const { asPath } = useRouter()
     if (asPath !== '/') {
