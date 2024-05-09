69716202d3625797724b64c1016e0c0123111089
Tom Calloway <modellurgist@gmail.com>
2024-05-09 13:02:07 +0100

    change title metadata

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
   logo: <span>Zero Coupled Architecture (ALA)</span>,
@@ -10,6 +11,14 @@ const config: DocsThemeConfig = {
   footer: {
     text: '© 2024 Tom Calloway (github.com/modellurgist)',
   },
+  useNextSeoProps: () => {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Zero Coupled'
+      }
+    }
+  }
 }
 
 export default config
